cea5add844bef395da8e99db1ce07399a68baa10
joshn1989 <joshua.nisbet1989@gmail.com>
2021-10-03 16:17:09 +0100

    Skip Command

---
+++ commands/skip.js
@@ -0,0 +1,16 @@
+module.exports = {
+        name: 'skip',
+        description: "An example command",
+        execute(interaction, client, player){
+            if (!interaction.member.voice.channelId) return interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
+            if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) 
+            return interaction.reply({ content: "You are not in my voice channel!",  ephemeral: true });
+
+            try {
+                queue.skip()
+            } catch {
+                return interaction.reply({ content: "Could not skip, ensure something is playing", ephemeral: true });
+            }       
+
+        }
+    }
